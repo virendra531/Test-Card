3ee59d29e62df59b051787665e9ff53ce13be116
Virendra Kumar <virendrakumar531@hotmail.com>
2025-02-27 20:24:52 +0530

    Implement scene loading and player preferences saving functionality

---
+++ Assets/Application/Scripts/SaveInPlayerPrefs.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class SaveInPlayerPrefs : MonoBehaviour
+{
+    public TMP_InputField inputFieldRow;
+    public TMP_InputField inputFieldColumn;
+
+    
+    public void SaveRow()
+    {
+        Save("row", inputFieldRow.text);
+    }
+    public void SaveColumn()
+    {
+        Save("column", inputFieldColumn.text);
+    }
+
+    public void Save(string key, string value)
+    {
+        PlayerPrefs.SetString(key, value);
+    }
+}
--- Assets/Application/Scripts/SceneLoad.cs
+++ Assets/Application/Scripts/SceneLoad.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class SceneLoad : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void LoadScene(string sceneName)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
     }
 }
